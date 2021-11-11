3cd211382eab5e3e808d13e6a9e3dee7e9671bb7
danielarriola <daniel.arriola@mercadolibre.com>
2021-11-11 20:30:05 -0300

    change port

--- API/index.js
+++ API/index.js
@@ -12,7 +12,12 @@ const requestsRouteHomeHealth = require("./routes/requests/homeHealth");
 const cors = require("cors");
 mongoose.set("useFindAndModify", false);
 require("dotenv/config");
-app.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 app.use(cors());
 mongoose.set("useCreateIndex", true);
 

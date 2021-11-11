27b393ad31c2d76aa74f7820220cf946c74d4660
danielarriola <daniel.arriola@mercadolibre.com>
2021-11-11 20:33:11 -0300

    heroku depl

--- API/index.js
+++ API/index.js
@@ -17,7 +17,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
-
 app.use(cors());
 mongoose.set("useCreateIndex", true);
 

4ff445f0d55eaa54679ea53d01cf6a1b98ef8d28
Daniel Arriola <darriola710@gmail.com>
2021-11-15 22:06:11 -0300

    allow token in header

--- API/index.js
+++ API/index.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
 });
-app.use(cors());
+app.use(
+  cors({
+    exposedHeaders: ["Auth-Token"],
+  })
+);
 mongoose.set("useCreateIndex", true);
 
 //Middlewares (Funciones)

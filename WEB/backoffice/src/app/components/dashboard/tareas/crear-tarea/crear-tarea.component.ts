f3d96e8c08cbfb8f7896fa06e5b6c77d19caf6ce
Joaco Bragança <43862239+joacobraganca@users.noreply.github.com>
2021-11-20 10:25:28 -0300

    Revert "Added task"

--- WEB/backoffice/src/app/components/dashboard/tareas/crear-tarea/crear-tarea.component.ts
+++ WEB/backoffice/src/app/components/dashboard/tareas/crear-tarea/crear-tarea.component.ts
@@ -1,27 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {
-  FormControl,
-  FormBuilder,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
 
 @Component({
   selector: 'app-crear-tarea',
   templateUrl: './crear-tarea.component.html',
-  styleUrls: ['./crear-tarea.component.sass'],
+  styleUrls: ['./crear-tarea.component.sass']
 })
 export class CrearTareaComponent implements OnInit {
-  public disabled = false;
-  public showSpinners = true;
 
-  public formGroup = new FormGroup({
-    date: new FormControl(null, [Validators.required]),
-  });
-  public dateControl = new FormControl(new Date());
+  constructor() { }
 
-  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+  }
 
-  ngOnInit() {}
 }

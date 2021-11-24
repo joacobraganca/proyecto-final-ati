13956482905a17287d9db10384d3a6321a1c3bbd
danielarriola <daniel.arriola@mercadolibre.com>
2021-11-24 13:12:53 -0300

    firebase config

--- MOBILE/app/App.js
+++ MOBILE/app/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import Navigation from './src/navigation';
-import SignInScreen from './src/screens/SignInScreen/SingInScreen';
+import {Alert} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <SafeAreaView style={styles.root}>
       <Navigation />

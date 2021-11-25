0e225ba6621b347f3a7672ac3dccebef07230496
Daniel Arriola <53195306+darriola7@users.noreply.github.com>
2021-11-25 17:18:14 -0300

    Feature/backoffice (#15)

--- WEB/backoffice/src/app/app-routing.module.ts
+++ WEB/backoffice/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AccessComponent } from './components/access/access.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthLoggedGuard } from './guards/auth-logged.guard';
+const routes: Routes = [
+  {
+    path: '',
+    component: AccessComponent,
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./components/dashboard/dashboard.module').then(
+        (x) => x.DashboardModule
+      ),
+  },
+  {
+    path: '**',
+    component: AccessComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

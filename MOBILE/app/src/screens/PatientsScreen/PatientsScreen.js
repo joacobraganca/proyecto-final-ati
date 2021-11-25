54110d76aff9bd2d049b6b1b52f161e865c75896
danielarriola <daniel.arriola@mercadolibre.com>
2021-11-25 12:57:54 -0300

    added search bar in patient screen

--- MOBILE/app/src/screens/PatientsScreen/PatientsScreen.js
+++ MOBILE/app/src/screens/PatientsScreen/PatientsScreen.js
@@ -1,16 +1,60 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, View, FlatList} from 'react-native';
 import CustomList from '../../components/Patient/CustomList/CustomList';
-import {getPatientsByHome} from '../../services/patient.service';
 import {connect} from 'react-redux';
+import {SearchBar} from 'react-native-elements';
 
 const PatientsScreen = ({patients}) => {
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState(patients);
+
+  const searchFilterFunction = text => {
+    if (text) {
+      const newData = patients.filter(function (item) {
+        const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      setFilteredDataSource(patients);
+      setSearch(text);
+    }
+  };
+
   return (
-    <View>
-      <CustomList patients={patients} />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <SearchBar
+          inputContainerStyle={{backgroundColor: 'white'}}
+          leftIconContainerStyle={{backgroundColor: 'white'}}
+          inputStyle={{backgroundColor: 'white'}}
+          containerStyle={{
+            backgroundColor: '#white',
+            justifyContent: 'space-around',
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+          }}
+          onChangeText={text => searchFilterFunction(text)}
+          onClear={text => searchFilterFunction('')}
+          placeholder="Escriba aqui..."
+          value={search}
+        />
+        <CustomList patients={filteredDataSource} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+  },
+});
 
 const mapStateToProps = state => {
   return {
